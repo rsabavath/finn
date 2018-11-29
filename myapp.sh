0d1ddf5829904bf612dff5f00d26d55a14c6ae4e
masiddy <42073812+masiddy@users.noreply.github.com>
2018-11-29 14:08:29 -0500

    mys

---
+++ myapp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ls -l 
+
+NAME=$(docker ps -aqf "name=appy")
+
+if [ -n "$NAME" ]; then
+
+    docker stop appy && docker rm appy
+fi
+
+docker build --rm -t appy .
+
+ID=$(docker images -q appy)
+
+docker tag $ID localhost:5000/appy:1
+
+docker run --name appy -d -p 3001:3000 localhost:5000/appy:1
+docker login --username=masiddy --email=masiddymaanikyam@gmail.com -p 111d60014
+
+
+docker tag $ID masiddy/appy:1
+docker push masiddy/appy:1
